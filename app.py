62d1fba4d970784df105faf977e444f8a7a982e7
CubeBeveled <cubebeveled@gmail.com>
2024-06-16 22:05:07 +0200

    Added a few more filtering endpoints

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, send_file, redirect
-from endpoints import wanted, welcomecard, sharpen
+from endpoints import wanted, welcomecard, sharpen, smooth, enhance, contour, find_edges
 
 fallback_avatar = "https://bevels-files.vercel.app/discordblue.png"
 app = Flask(__name__)
@@ -42,12 +42,77 @@ def generate_sharp():
   
   if image_url == None:
     return redirect(f"/sharpen?image=https://bevels-files.vercel.app/bsl.png&cycles=1")
+  
   if cycles == None:
     return redirect(f"/sharpen?image=https://bevels-files.vercel.app/bsl.png&cycles=1")
   
   print(f"Generating sharpened image ({cycles}x)")
   
   return send_file(sharpen.generate(image_url, cycles), mimetype="image/png")
+
+@app.route("/smooth", methods=["GET"])
+def generate_smooth():
+  
+  image_url = request.args.get("image")
+  cycles = request.args.get("cycles")
+  
+  if image_url == None:
+    return redirect(f"/smooth?image=https://bevels-files.vercel.app/bsl.png&cycles=1")
+  
+  if cycles == None:
+    return redirect(f"/smooth?image=https://bevels-files.vercel.app/bsl.png&cycles=1")
+  
+  print(f"Generating smoothed image ({cycles}x)")
+  
+  return send_file(smooth.generate(image_url, cycles), mimetype="image/png")
+
+@app.route("/edge_enhance", methods=["GET"])
+def generate_enhanced():
+  
+  image_url = request.args.get("image")
+  cycles = request.args.get("cycles")
+  
+  if image_url == None:
+    return redirect(f"/edge_enhance?image=https://bevels-files.vercel.app/bsl.png&cycles=1")
+  
+  if cycles == None:
+    return redirect(f"/edge_enhance?image=https://bevels-files.vercel.app/bsl.png&cycles=1")
+  
+  print(f"Generating enhanced image ({cycles}x)")
+  
+  return send_file(enhance.generate(image_url, cycles), mimetype="image/png")
+
+@app.route("/contour", methods=["GET"])
+def generate_contour():
+  
+  image_url = request.args.get("image")
+  cycles = request.args.get("cycles")
+  
+  if image_url == None:
+    return redirect(f"/contour?image=https://bevels-files.vercel.app/bsl.png&cycles=1")
+  
+  if cycles == None:
+    return redirect(f"/contour?image=https://bevels-files.vercel.app/bsl.png&cycles=1")
+  
+  print(f"Generating contoured image ({cycles}x)")
+  
+  return send_file(contour.generate(image_url, cycles), mimetype="image/png")
+
+@app.route("/find_edges", methods=["GET"])
+def generate_edge():
+  
+  image_url = request.args.get("image")
+  cycles = request.args.get("cycles")
+  
+  if image_url == None:
+    return redirect(f"/find_edges?image=https://bevels-files.vercel.app/bsl.png&cycles=1")
+  
+  if cycles == None:
+    return redirect(f"/find_edges?image=https://bevels-files.vercel.app/bsl.png&cycles=1")
+  
+  print(f"Generating edged image ({cycles}x)")
+  
+  return send_file(find_edges.generate(image_url, cycles), mimetype="image/png")
 
 if __name__ == "__main__":
   app.run(host="0.0.0.0", debug=False, port=25265)
---
+++ endpoints/contour.py
@@ -0,0 +1,22 @@
+from PIL import Image, ImageFilter
+from io import BytesIO
+import requests
+from requests.exceptions import RequestException
+
+def generate(image_url, cycles):
+  try:
+    response = requests.get(image_url)
+    response.raise_for_status()
+    image = Image.open(BytesIO(response.content))
+    
+    for i in range(int(cycles)):
+      image = image.filter(ImageFilter.CONTOUR)
+  except RequestException as e:
+    print(f"Error downloading background image: {e}")
+  
+  # Convert to buffer
+  buffer = BytesIO()
+  image.save(buffer, format="PNG")
+  buffer.seek(0)
+  
+  return buffer
